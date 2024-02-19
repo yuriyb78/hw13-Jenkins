e6f04cd44795f908ee2dcb37ec6755a8cb47ea76
Yuriy Belyaev <byur78@mail.ru>
2024-02-19 16:53:15 +0300

    edit TestData

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -4,18 +4,39 @@ import utils.GenUserData;
 
 public class TestData {
 
-    public String firstName = GenUserData.getRandomFirstName(),
-            lastName = GenUserData.getRandomLastName(),
-            emailAddress = GenUserData.getRandomEmailAddress(),
-            genderValue = GenUserData.getRandomGenderValue(),
-            phoneNumber = GenUserData.getRandomPhoneNumber(),
-            dayOfBirthDay = String.valueOf(GenUserData.getRandomDayOfBirthDay()),
-            monthOfBirthDay = GenUserData.getRandomMonthOfBirthDay(dayOfBirthDay),
-            yearOfBirthDay = String.valueOf(GenUserData.getRandomYearOfBirthDay()),
-            subjectChar = GenUserData.getRandomGetRandomSubject(),
-            hobbieValue = GenUserData.getRandomHobbies(),
-            namePicture = "01.jpg",
-            fullAddress = GenUserData.getRandomFullAddress(),
-            nameState = GenUserData.getRandomNameState(),
+    public String firstName,
+            lastName,
+            emailAddress,
+            genderValue,
+            phoneNumber,
+            dayOfBirthDay,
+            monthOfBirthDay,
+            yearOfBirthDay,
+            subjectChar,
+            hobbieValue,
+            namePicture,
+            fullAddress,
+            nameState,
+            nameCity;
+
+    public TestData() {
+        try {
+            firstName = GenUserData.getRandomFirstName();
+            lastName = GenUserData.getRandomLastName();
+            emailAddress = GenUserData.getRandomEmailAddress();
+            genderValue = GenUserData.getRandomGenderValue();
+            phoneNumber = GenUserData.getRandomPhoneNumber();
+            dayOfBirthDay = String.valueOf(GenUserData.getRandomDayOfBirthDay());
+            monthOfBirthDay = GenUserData.getRandomMonthOfBirthDay(dayOfBirthDay);
+            yearOfBirthDay = String.valueOf(GenUserData.getRandomYearOfBirthDay());
+            subjectChar = GenUserData.getRandomGetRandomSubject();
+            hobbieValue = GenUserData.getRandomHobbies();
+            namePicture = "01.jpg";
+            fullAddress = GenUserData.getRandomFullAddress();
+            nameState = GenUserData.getRandomNameState();
             nameCity = GenUserData.getRandomNameCity(nameState);
+        } catch (Throwable e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
